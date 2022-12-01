f67dbd16f64fa80a2fe4822503294630b6c5938e
Flawlessgod <christophequentingerard@gmail.com>
2022-11-30 22:58:00 -0600

    create navbar and navbar.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/portfolio" element={<Home />} />
           <Route path="/portfolio/projects" element={<Projects />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  return (
+    // if navbar is open else its closed
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      Navbar
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            // reverse whatever the last boolean value was
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/portfolio">Home</Link>
+        <Link to="/portfolio/projects">Projects</Link>
+        <Link to="/portfolio/experience">Experience</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
