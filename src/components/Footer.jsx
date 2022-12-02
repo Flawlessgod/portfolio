5bd7233b875fd08dfba2d390597a1aa6e0999db6
Flawlessgod <christophequentingerard@gmail.com>
2022-12-02 13:27:18 -0600

    content home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../styles/Footer.css";
+
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import Resume from "@mui/icons-material/Article";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
         <LinkedInIcon />
         <Resume />
+        <EmailIcon />
+        <GitHubIcon />
       </div>
       <p>&copy; 2022 Christophe Gerard</p>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import "../styles/Home.css";
+
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Resume from "@mui/icons-material/Article";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <div className="about">
+        <h2>Hello, I am Christophe</h2>
+        <div className="prompt">
+          <p>
+            Welcome! I am a self-driven and task-oriented third year computer
+            science student{" "}
+          </p>
+          <LinkedInIcon />
+          <Resume />
+          <EmailIcon />
+          <GitHubIcon />
+        </div>
+      </div>
+
+      <div className="skills">
+        <ol className="list">
+          <li className="item">
+            <div className="skill"> Skills</div>
+          </li>
+        </ol>
+        {/* ordered list  */}
+        <ol className="list">
+          {/* list */}
+          <li className="item">
+            <div>Front-End</div>
+            <span>React, CSS, HTML</span>
+          </li>
+        </ol>
+
+        <div className="skills">
+          <ol className="list">
+            <li className="item">
+              <div>Back-End</div>
+              <span>N/A</span>
+            </li>
+          </ol>
+        </div>
+
+        <div className="skills">
+          <ol className="list">
+            <li className="item">
+              <div>Languages</div>
+              <span>Java, Python, Javascript</span>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
