c4185b3a16336829dfa89ef1b0b78040cfd9dfa1
Flawlessgod <christophequentingerard@gmail.com>
2022-12-15 12:52:55 -0600

    visualize Experiences

---
+++ src/helpers/ProjectList.jsx
@@ -0,0 +1,8 @@
+import proj1 from "../assets/corrupt.jpg";
+
+export const projectList = [
+  {
+    name: "dijkstra's algorithm visualizer",
+    image: proj1,
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import PersonalProject from "../components/ProjectItems";
+import "../styles/Projects.css";
+//import proj1 from "../assets/proj1.jpg";
 
+import { projectList } from "../helpers/ProjectList";
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectlist">
+        {projectList.map((project) => {
+          return <PersonalProject name={project.name} image={project.image} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
